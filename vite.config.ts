80129f4326752f60b15f7572986f35062ffa8770
aizeroin <admin@aizero.in>
2026-02-01 03:35:01 -0500

    website update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "https://tamilari.github.io/",
+   base: "https://www.tamilari.com/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
